fb14c44f3276668802396c0d7fe991e5dacaec08
vitaliy <k.vitaliy1994@gmail.com>
2018-02-03 21:23:57 +0200

    fix content-length, send in HEX

--- sketch1/sketch1.ino
+++ sketch1/sketch1.ino
@@ -53,7 +53,7 @@ void doPost()
           char buf[100];
           Serial.println("Client connected");
           char a[] = "123";
-           sprintf(buf, "POST /post_test HTTP/1.0\r\nContent-Type: application/raw\r\nContent-Length: 2\r\n\r\n%d", serNum0);
+           sprintf(buf, "POST /post_test HTTP/1.0\r\nContent-Type: application/raw\r\nContent-Length: 14\r\n\r\n%X %X %X %X %X", serNum0, serNum1, serNum2, serNum3, serNum4);
            Serial.println(buf);
            delay(1000);
            client.println(buf); // Отправляем GET запрос
